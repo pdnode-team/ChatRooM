198ee076d5c5ac5b3a8ec3f67439974edd03c66e
XxdMkbMark <103048057+XxdMkbMark@users.noreply.github.com>
2025-07-28 19:53:48 +0800

    import mui icons

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,11 @@
-import React from "react";
+import * as React from 'react';
+import { styled, alpha } from '@mui/material/styles';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import SearchIcon from '@mui/icons-material/Search';
+import TextField from '@mui/material/TextField';
 
 export default function Home() {
   return (
