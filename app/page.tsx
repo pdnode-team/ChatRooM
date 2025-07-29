63dd4b85f6e38ac97f84d3275851594dcddd1c1b
XxdMkbMark <103048057+XxdMkbMark@users.noreply.github.com>
2025-07-29 18:07:33 +0800

    add icon

--- app/page.tsx
+++ app/page.tsx
@@ -6,13 +6,15 @@ import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
 import SearchIcon from '@mui/icons-material/Search';
 import TextField from '@mui/material/TextField';
+import Image from 'next/image';
 
 export default function Home() {
   return (
     <div>
       <title>ChatRooM V2</title>
-      <AppBar position="static">
-        <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
+      <AppBar position="static" className='appbar-home' sx={{background: "#1976d2", height: '8vh', width: '100vw'}}>
+        <Image src="/icon.png" width="207" height="50" alt='ChatRooM Logo' priority style={{position: 'absolute', left: '1vw', top: '0.9vh'}}/>
+        <Box sx={{ display: 'flex', alignItems: 'flex-end', position: 'absolute', right: '1%', top: '0.9vh'}}>
           <SearchIcon sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
         <TextField id="search-owned-server" label="Search" variant="standard" />
       </Box>
