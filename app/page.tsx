0b20b4140f2f6f036e9b8c335a10d73565bea84d
XxdMkbMark <103048057+XxdMkbMark@users.noreply.github.com>
2025-07-29 18:21:24 +0800

    add themeprovider

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,53 @@
 import * as React from 'react';
-import { styled, alpha } from '@mui/material/styles';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import SearchIcon from '@mui/icons-material/Search';
 import TextField from '@mui/material/TextField';
 import Image from 'next/image';
+import Link from 'next/link';
+
+declare module '@mui/material/styles' {
+  interface Palette {
+    ochre: Palette['primary'];
+  }
+
+  interface PaletteOptions {
+    ochre?: PaletteOptions['primary'];
+  }
+}
+
+// Update the Button's color options to include an ochre option
+declare module '@mui/material/Button' {
+  interface ButtonPropsColorOverrides {
+    ochre: true;
+  }
+}
+
+const theme = createTheme({
+  palette: {
+    ochre: {
+      main: '#E3D026',
+      light: '#E9DB5D',
+      dark: '#A29415',
+      contrastText: '#242105',
+    },
+  },
+});
 
 export default function Home() {
   return (
-    <div>
-      <title>ChatRooM V2</title>
-      <AppBar position="static" className='appbar-home' sx={{background: "#1976d2", height: '8vh', width: '100vw'}}>
-        <Image src="/icon.png" width="207" height="50" alt='ChatRooM Logo' priority style={{position: 'absolute', left: '1vw', top: '0.9vh'}}/>
-        <Box sx={{ display: 'flex', alignItems: 'flex-end', position: 'absolute', right: '1%', top: '0.9vh'}}>
-          <SearchIcon sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
-        <TextField id="search-owned-server" label="Search" variant="standard" />
-      </Box>
-      </AppBar>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div>
+        <title>ChatRooM V2</title>
+        <AppBar position="static" className='appbar-home'>
+          <Link href="/"><Image src="/icon.png" className='logo-icon' width="207" height="50" alt='ChatRooM Logo' priority/></Link>
+          <Box sx={{ display: 'flex', alignItems: 'flex-end', position: 'absolute', right: '1%', top: '0.9vh'}}>
+            <SearchIcon sx={{color: 'action.active', mr: 1, my: 0.5}} />
+          <TextField color='#ffffff' id="joined-server-search" label="Search" variant="standard"/>
+        </Box>
+        </AppBar>
+      </div>
+    </ThemeProvider>
   );
 }
