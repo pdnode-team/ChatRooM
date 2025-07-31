6fd91a70b8c2f072e6b6c46f59dda67b1b562232
XxdMkbMark <103048057+XxdMkbMark@users.noreply.github.com>
2025-07-31 20:55:07 +0800

    added reg

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -75,11 +75,11 @@ export default function Home() {
           <LoginIcon sx={{color: "#1976d2", fontSize: '65px', marginTop: '70px'}}/>
           <Typography variant="h4">Login to your account</Typography>
           <Box sx={{display: 'flex', alignItems: 'center', justifyContent: 'center', width: '100%', marginTop: '10px'}}>
-            <UsernameIcon sx={{fontSize: '30px', marginRight: '10px'}}/>
+            <UsernameIcon sx={{fontSize: '35px', marginRight: '10px'}}/>
             <TextField label="Username" variant="filled" type="username" sx={{width: '60%'}}/>
           </Box>
           <Box sx={{display: 'flex', alignItems: 'center', justifyContent: 'center', width: '100%', marginTop: '10px'}}>
-            <PasswordIcon sx={{fontSize: '30px', marginRight: '10px'}}/>
+            <PasswordIcon sx={{fontSize: '35px', marginRight: '10px'}}/>
             <TextField label="Password" variant="filled" type="password" sx={{width: '60%'}}/>
           </Box>
           <Button variant="contained" sx={{width: '40%', height: '50px', marginTop: '15px', boxShadow: 'none', fontSize: '15px'}}>Login</Button>
