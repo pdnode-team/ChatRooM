980dd2aa76cb4a8a725b47e1f350bbfb50e6cfe5
XxdMkbMark <103048057+XxdMkbMark@users.noreply.github.com>
2025-08-06 12:38:12 +0800

    why is this so fuxxing hard

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -18,11 +18,15 @@ import Typography from "@mui/material/Typography";
 import UsernameIcon from '@mui/icons-material/Person';
 import { useState } from "react";
 import { useRouter } from "next/router";
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
 
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import { InputAdornment, inputAdornmentClasses } from "@mui/material";
+import { Label } from "@mui/icons-material";
 
 
 //别管这是干啥的，把要添加的新颜色写在这里面就行
@@ -134,17 +138,31 @@ export default function Login() {
       </Box>
       <div className='login-container'>
         <div className="login-content">
-          <LoginIcon sx={{color: "#1976d2", fontSize: '65px', marginTop: '70px'}}/>
-          <Typography variant="h4">Login to your account</Typography>
-          <Box sx={{display: 'flex', alignItems: 'center', justifyContent: 'center', width: '100%', marginTop: '10px'}}>
-            <UsernameIcon sx={{color: "#747474", fontSize: '30px', marginRight: '10px'}}/>
-            <TextField label="Username" variant="filled" type="username" sx={{width: '60%'}}/>
-          </Box>
-          <Box sx={{display: 'flex', alignItems: 'center', justifyContent: 'center', width: '100%', marginTop: '10px'}}>
-            <PasswordIcon sx={{color: "#747474", fontSize: '30px', marginRight: '10px'}}/>
-            <TextField label="Password" variant="filled" type="password" sx={{width: '60%'}}/>
-          </Box>
-          <Button variant="contained" sx={{width: '33%', height: '48px', marginTop: '15px', boxShadow: 'none', fontSize: '15px'}}>Login</Button>
+          <form onSubmit={handleSubmit}>
+            <LoginIcon sx={{color: "#1976d2", fontSize: '65px', marginTop: '70px'}}/>
+            <Typography variant="h4">Login to your account</Typography>
+            <Box sx={{display: 'flex', alignItems: 'center', justifyContent: 'center', width: '100%', marginTop: '10px'}}>
+              <UsernameIcon sx={{color: "#747474", fontSize: '30px', marginRight: '10px'}}/>
+              <TextField label="Username" variant="filled" type="username" sx={{width: '60%'}} value={formData.username} onChange={handleChange}/>
+            </Box>
+            <Box sx={{display: 'flex', alignItems: 'center', justifyContent: 'center', width: '100%', marginTop: '10px'}}>
+              <PasswordIcon sx={{color: "#747474", fontSize: '30px', marginRight: '10px'}}/>
+              <TextField label="Password" variant="filled" type={formData.showPassword ? 'text' : 'password'} sx={{width: '60%'}} value={formData.password} onChange={handleChange}
+              slotProps={{
+                input: {
+                  style:{color: '#747474'},
+                  endAdornment: (
+                    <InputAdornment position="end">
+                      <IconButton onClick={handleShowPassword} edge="end" sx={{color: '#747474'}}>
+                        {formData.showPassword ? <VisibilityOffIcon /> : <VisibilityIcon />}
+                      </IconButton>
+                    </InputAdornment>
+                  ),
+                },
+              }}/>
+            </Box>
+            <Button variant="contained" sx={{width: '33%', height: '48px', marginTop: '15px', boxShadow: 'none', fontSize: '15px'}}>Login</Button>
+          </form>
           <Typography sx={{marginTop: '10px'}}>Haven&apos;t got an account yet? <Link href="/register" style={{color: '#006fdeff', textDecoration: 'underline'}}>Create an account.</Link></Typography>
         </div>
       </div>
